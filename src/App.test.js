05d111a2dec4e302201893a87cc2eab3af6208a1
NishkarshRaj <nishkarshraj000@gmail.com>
2023-10-11 12:45:06 +0530

    Fixing test cases
    Signed-off-by: NishkarshRaj <nishkarshraj000@gmail.com>

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Follow me on GitHub/i);
   expect(linkElement).toBeInTheDocument();
 });
